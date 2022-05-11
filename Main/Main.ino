0dc65fbfd743746ca95a793788d9a69f1f6fb1bb
rswe715 <rswe715@aucklanduni.ac.nz>
2022-05-11 15:59:19 +1200

    Adding Battery class to check battery, and some changes torward Serials

--- Main/Main.ino
+++ Main/Main.ino
@@ -4,7 +4,7 @@
 #define BLUETOOTH_RX 10
 // Serial Data output pin
 #define BLUETOOTH_TX 11
-SoftwareSerial BluetoothSerial(BLUETOOTH_RX, BLUETOOTH_TX);
+SoftwareSerial *BluetoothSerial = new SoftwareSerial(BLUETOOTH_RX, BLUETOOTH_TX);
 ///////////////////////////////////////////////////////////////////////////
 
 //file includes
@@ -13,6 +13,7 @@ SoftwareSerial BluetoothSerial(BLUETOOTH_RX, BLUETOOTH_TX);
 #include "Gyro.h"
 #include "SonarSensor.h"
 #include "IRRangePair.h"
+#include "Battery.h"
 
 enum STATE {
   INITIALISING,
@@ -24,7 +25,7 @@ enum STATE {
 
 STATE state;
 float deltaT = 1; //Running Period
-//HardwareSerial *SerialCom;
+HardwareSerial *SerialCom;
 
 Chassis* chassis;
 Turret* turret;
@@ -32,6 +33,7 @@ Gyro* gyro;
 SonarSensor* sonarSensor;
 IRRangePair* sideRangePair;
 IRRangePair* forwardRangePair;
+Battery* battery;
 
 void setup()
 {
@@ -43,13 +45,13 @@ void setup()
   sonarSensor = new SonarSensor();
   sideRangePair = new IRRangePair(A15, A13, 10); // fix these vals
   forwardRangePair = new IRRangePair(A14, A11, 10);
+  battery = new Battery();
 
   //Serial Pointer
   // Setup the Serial port and pointer, the pointer allows switching the debug info through the USB port(Serial) or Bluetooth port(Serial1) with ease.
-  /*SerialCom = &Serial;
-  SerialCom->begin(115200);*/
-  BluetoothSerial.begin(115200);
-  //SerialCom->println("ROBUSSY ROLLOUT");
+  SerialCom->begin(115200);
+  BluetoothSerial->begin(115200);
+  SerialCom->println("ROBUSSY ROLLOUT");
 }
 
 void loop()
@@ -66,11 +68,16 @@ void loop()
 
 void Initialising(float deltaT)
 {
+  // Check Battery
+  battery->Check();
 
+  // Begin tasks
+  state = DRIVING;
 }
 
 void Driving(float deltaT)
 {
+  SerialCom->println("SSSSSSSSS");
   chassis->SetSpeed(100, 0, 0);
   chassis->Run(deltaT);
   sonarSensor->Run();
